40b40fea3b4d5686dd679e6ef9d269c4672b1065
Guillem <guillemburguesmiro@gmail.com>
2023-03-14 11:04:22 +0100

    Error handling (WIP)

--- AppController.py
+++ AppController.py
@@ -1,7 +1,7 @@
 from Trakt.TraktAPI import TraktAPI
 from Database.DatabaseManager import DatabaseManager
 from Utils.Logger import Logger
-import Utils.Trakt as Trakt
+import Utils.StatusCodes as StatusCodes
 import json
 
 
@@ -10,33 +10,49 @@ class AppController():
     def __init__(self, showLog=True):
         self.logger = Logger(showLog, "CONTROLLER")
         self.traktConfig = self.__LoadTraktConfig()
-        self.trakt = TraktAPI(showLog, self.traktConfig['clientID'])
+        self.trakt = TraktAPI(showLog)
         self.database = DatabaseManager(showLog)
 
 
     def AuthorizeTraktUser(self):
         if not self.__TraktUserAuthorized():
             result = self.trakt.AuthorizeUser(self.traktConfig['clientID'], self.traktConfig['clientSecret'])
-            if (result['code'] == Trakt.SUCCESS):
+            if (result['code'] == StatusCodes.TRAKT_SUCCESS):
                 self.traktConfig['accessToken'] = result['accessToken']
                 self.traktConfig['refreshToken'] = result['refreshToken']
                 self.__SaveConfig()
             return result['code']
-        return Trakt.SUCCESS
+        else:
+            message = "User already authorized"
+            self.logger.ShowMessage(message)
+
+        return StatusCodes.TRAKT_SUCCESS
     
     def BackupWatchedHistory(self):
+        if (not self.__TraktUserAuthorized()):
+            message = "User is not authorized (missing access token). Error: {} {}".format(StatusCodes.TRAKT_UNAUTHORIZED, StatusCodes.statusMessages[StatusCodes.TRAKT_UNAUTHORIZED])
+            self.logger.ShowMessage(message)
+            return StatusCodes.TRAKT_UNAUTHORIZED
+        
         page = 1
         syncing = True
-        statusCode = Trakt.SUCCESS
+        statusCode = StatusCodes.TRAKT_SUCCESS
+        totalPlays = 0
         self.database.OpenDatabase()
-        while (syncing and statusCode == Trakt.SUCCESS):
-            statusCode, data = self.trakt.GetHistoryPage(page, self.traktConfig['accessToken'])
-            if (statusCode == Trakt.SUCCESS):
+        while (syncing and statusCode == StatusCodes.TRAKT_SUCCESS):
+            statusCode, data = self.trakt.GetHistoryPage(page, self.traktConfig['clientID'], self.traktConfig['accessToken'])
+            if (statusCode == StatusCodes.TRAKT_SUCCESS):
+                message = "Processing plays from page {}".format(page)
+                self.logger.ShowMessage(message)
                 syncing, statusCode = self.__ProcessTraktPlays(data)
-            page += 1
+                totalPlays += len(data)
+                page += 1
         
         self.database.CloseDatabase()
-        message = "Process finished. Code result: " + str(statusCode) + " " + Trakt.statusMessages[statusCode]
+        if (statusCode != StatusCodes.TRAKT_SUCCESS):
+            message = "Backup history finished with an error. {} pages were processed. Check previous logs for more info".format(page - 1)
+        else:
+            message = "Backup history finished. Pages processed: {}. Total plays found: {}".format(page - 1, totalPlays)
         self.logger.ShowMessage(message)
 
         return statusCode
@@ -63,36 +79,40 @@ class AppController():
         for play in plays:
             self.database.AddPlay(play)
 
-        syncing = True if len(plays) == Trakt.SYNC_MAX_LIMIT else False
+        syncing = True if len(plays) == self.trakt.GetSyncLimit() else False
 
-        return syncing, Trakt.SUCCESS
+        return syncing, StatusCodes.TRAKT_SUCCESS
 
     def __TraktUserAuthorized(self):
-        if (self.traktConfig['accessToken'] != ''):
-            message = "User already authorized"
-            self.logger.ShowMessage(message)
+        if (len(self.traktConfig['accessToken']) == '64'):
             return True
         else:
             return False
 
     def __LoadTraktConfig(self):
-        with open('settings.json', 'r') as configFile:
-            data = json.load(configFile)
-            traktConfig = {
-                'clientID' : data['clientID'],
-                'clientSecret' : data['clientSecret'],
-                'accessToken' : data['accessToken'],
-                'refreshToken' : data['refreshToken']
-            }
-
+        try:
+            with open('settings.json', 'r') as configFile:
+                data = json.load(configFile)
+                traktConfig = {
+                    'clientID' : data['clientID'],
+                    'clientSecret' : data['clientSecret'],
+                    'accessToken' : data['accessToken'],
+                    'refreshToken' : data['refreshToken']
+                }
             message = "Trakt config loaded"
+        except Exception as err:
+            message = "An error occurred when loading the settings file. {}".format(err)
+            traktConfig = {'clientID' : "", 'clientSecret' : "", 'accessToken' : "", 'refreshToken' : ""}
+        finally:
             self.logger.ShowMessage(message)
-
-        return traktConfig
+            return traktConfig
 
     def __SaveConfig(self):
-        with open('settings.json', 'w') as configFile:
-            json.dump(self.traktConfig, configFile)
-
-        message = "Trakt config saved"
-        self.logger.ShowMessage(message)
+        try:
+            with open('settings.json', 'w') as configFile:
+                json.dump(self.traktConfig, configFile)
+            message = "Trakt config saved"
+        except Exception as err:
+            message = "An error occurred when saving the settings file. {}".format(err)
+        finally:
+            self.logger.ShowMessage(message)
--- Trakt/TraktAPI.py
+++ Trakt/TraktAPI.py
@@ -1,5 +1,5 @@
 from Utils.Logger import Logger
-import Utils.Trakt as Trakt
+import Utils.StatusCodes as StatusCodes
 import requests
 import time
 
@@ -8,13 +8,10 @@ class TraktAPI:
 
     baseURL = "https://api.trakt.tv"
     traktAPIVersion = '2'
+    
+    TRAKT_SYNC_LIMIT = 1000
 
-    def __init__(self, showLog, clientID):
-        self.headers = {
-            'Content-type': 'application/json',
-            'trakt-api-key': clientID,
-            'trakt-api-version': self.traktAPIVersion
-        }
+    def __init__(self, showLog):
         self.logger = Logger(showLog, "TRAKT")
 
     def AuthorizeUser(self, clientID, clientSecret):
@@ -22,57 +19,84 @@ class TraktAPI:
         authorizeHeader = {'Content-type': 'application/json'}
 
         postData = {"client_id" : clientID}
+        try:
+            response = requests.post(self.baseURL + '/oauth/device/code', json=postData, headers=authorizeHeader)
 
-        response = requests.post(self.baseURL + '/oauth/device/code', json=postData, headers=authorizeHeader)
-
-        if (response.status_code == Trakt.SUCCESS):
-            authorizationData = response.json()
-            message = "Go to {} in your browser and input the following code: {}".format(authorizationData['verification_url'], authorizationData['user_code'])
-            self.logger.ShowMessage(message)
-
-            tokenData = {
-                "code" : authorizationData['device_code'],
-                'client_id' : clientID,
-                'client_secret' : clientSecret
-            }
-
-            url = self.baseURL + '/oauth/device/token'
-            tokenResponse = requests.post(url , json=tokenData, headers=authorizeHeader)
-
-            pullTime = 0
-
-            while (tokenResponse.status_code != Trakt.SUCCESS and pullTime < authorizationData['expires_in']) :
-                time.sleep(authorizationData['interval'])
-                pullTime += authorizationData['interval']
-                tokenResponse = requests.post(url, json=tokenData, headers=authorizeHeader)
-                message = "Response code: {}".format(tokenResponse.status_code)
+            if (response.status_code == StatusCodes.TRAKT_SUCCESS):
+                authorizationData = response.json()
+                message = "Go to {} in your browser and input the following code: {}".format(authorizationData['verification_url'], authorizationData['user_code'])
                 self.logger.ShowMessage(message)
 
-            if (tokenResponse.status_code != Trakt.SUCCESS) : 
-                message = "Could not authorize user. Code: {} {}".format(response.status_code, Trakt.statusMessages[response.status_code])
-                self.logger.ShowMessage(message)
-                return {'accessToken' : None, 'refresh_token' : None, 'code' : tokenResponse.status_code}
+                tokenData = {
+                    "code" : authorizationData['device_code'],
+                    'client_id' : clientID,
+                    'client_secret' : clientSecret
+                }
+
+                url = self.baseURL + '/oauth/device/token'
+                tokenResponse = requests.post(url , json=tokenData, headers=authorizeHeader)
+
+                pullTime = 0
+
+                while (tokenResponse.status_code != StatusCodes.TRAKT_SUCCESS and pullTime < authorizationData['expires_in']) :
+                    time.sleep(authorizationData['interval'])
+                    pullTime += authorizationData['interval']
+                    tokenResponse = requests.post(url, json=tokenData, headers=authorizeHeader)
+                    message = "Response code: {}".format(tokenResponse.status_code)
+                    self.logger.ShowMessage(message)
+
+                if (tokenResponse.status_code != StatusCodes.TRAKT_SUCCESS) : 
+                    message = "Could not authorize user. Code: {} {}".format(response.status_code, StatusCodes.statusMessages[response.status_code])
+                    self.logger.ShowMessage(message)
+                    return {'accessToken' : None, 'refresh_token' : None, 'code' : tokenResponse.status_code}
+                else:
+                    tokenData = tokenResponse.json()
+                    return {'accessToken' : tokenData['access_token'], 'refresh_token' : tokenData['refresh_token'], 'code' : tokenResponse.status_code}
             else:
-                tokenData = tokenResponse.json()
-                return {'accessToken' : tokenData['access_token'], 'refresh_token' : tokenData['refresh_token'], 'code' : tokenResponse.status_code}
-        else:
-            message = "Could not authorize user. Code: {} {}".format(response.status_code, Trakt.statusMessages[response.status_code])
+                message = "Could not authorize user. Code: {} {}".format(response.status_code, StatusCodes.statusMessages[response.status_code])
+                self.logger.ShowMessage(message)
+                return {'accessToken' : None, 'refresh_token' : None, 'code' : response.status_code}
+            
+        except requests.exceptions.RequestException as err:
+            message = "An error occurred in requests module: {}".format(err)
             self.logger.ShowMessage(message)
-            return response.status_code
+            return {'accessToken' : None, 'refresh_token' : None, 'code' : StatusCodes.REQUESTS_ERROR}
+
         
-    def GetHistoryPage(self, page, accessToken):
-
-        message = "Requesting history page {}".format(page)
-        self.logger.ShowMessage(message)
+    def GetHistoryPage(self, page, clientID, accessToken):
         
-        historyHeader = self.headers
-        historyHeader['Authorization'] = "Bearer " + accessToken
+        historyHeader = self.__BuildRequiredHeader(clientID, accessToken)
 
-        response = requests.get(self.baseURL + '/sync/history?extended=full&page={}&limit={}'.format(page, Trakt.SYNC_MAX_LIMIT), headers=historyHeader)
+        data = None
+        message = ""
+        statusCode = ""
 
-        if response.status_code == Trakt.SUCCESS :
-            return response.status_code, response.json()
-        else:
-            message = "Could not download page. Code: {} {}".format(response.status_code, Trakt.statusMessages[response.status_code])
+        try:
+            response = requests.get(self.baseURL + '/sync/history?extended=full&page={}&limit={}'.format(page, self.TRAKT_SYNC_LIMIT), headers=historyHeader)
+            statusCode = response.status_code
+
+            if (statusCode == StatusCodes.TRAKT_SUCCESS):
+                message = "Downloaded history page {}".format(page)
+                data = response.json()
+            else:
+                message = "Could not download page. Code: {} {}".format(statusCode, StatusCodes.statusMessages[statusCode])
+
+        except requests.exceptions.RequestException as err:
+            message = "An error occurred in requests module: {}".format(err)
+            statusCode = StatusCodes.REQUESTS_ERROR
+            
+        finally:
             self.logger.ShowMessage(message)
-            return response.status_code, None
+            return statusCode, data
+
+    def GetSyncLimit(self):
+        return self.TRAKT_SYNC_LIMIT
+        
+    def __BuildRequiredHeader(self, clientID, accessToken):
+        header = {
+            'Content-type': 'application/json',
+            'trakt-api-key': clientID,
+            'trakt-api-version': self.traktAPIVersion,
+            'Authorization': "Bearer " + accessToken
+        }   
+        return header
